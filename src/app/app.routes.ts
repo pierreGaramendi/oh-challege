e339b7b9a186c62eac56e1aa97f6657296f9a48a
Pierre Garamendi <pierre.garamendi@gmail.com>
2024-09-12 16:38:33 -0500

    frist commi: app structure begin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./presentation/layout/layout.component').then(
+        (m) => m.LayoutComponent
+      ),
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./presentation/pages/home/home.component').then(
+            (m) => m.HomeComponent
+          ),
+      },
+    ],
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./presentation/pages/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./presentation/pages/signup/signup.component').then(
+        (m) => m.SignupComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
